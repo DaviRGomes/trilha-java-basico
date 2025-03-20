804f33a1a31029981fbec54fb01e0b64a4476233
Davi Gomes <davirosagomesvini@gmail.com>
2025-03-20 14:52:43 -0300

    Adicionando o desafio poo iPhone

---
+++ desafios/poo/desafioPooIphone/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        // Testando Reprodutor Musical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+
+        // Testando Aparelho Telefônico
+        meuIphone.ligar("1234-5678");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando Navegador na Internet
+        meuIphone.exibirPagina("https://www.google.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
